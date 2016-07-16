abb6c1584daac7614e656af72582897f2d8fb3ec
chriszhangjie <13601115771@163.com>
2016-07-16 22:55:16 +0800

    框架的搭建
    框架搭建…

---
+++ MiaoLive/MiaoLive/Home/HomeViewController.m
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.m
+//  MiaoLive
+//
+//  Created by 张杰 on 16/7/16.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+}
+@end
